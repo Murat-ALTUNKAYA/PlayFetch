4427a37cea6f762677c81bde1c693bb3a236bf18
Murat Altunkaya <murataltunkaya7@gmail.com>
2022-03-10 14:37:17 +0300

    SecondCommit

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -9,12 +9,10 @@ public class DestroyOutOfBoundsX : MonoBehaviour
 
     void Update()
     {
-        // Destroy dogs if x position less than left limit
         if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
-        // Destroy balls if y position is less than bottomLimit
         else if (transform.position.y < bottomLimit)
         {
             Destroy(gameObject);
--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
+using System;
 
 public class PlayerControllerX : MonoBehaviour
 {
