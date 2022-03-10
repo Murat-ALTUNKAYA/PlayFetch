e3632917fbfd2f1560f1f4e3ff2a861ff8dd7858
Murat Altunkaya <murataltunkaya7@gmail.com>
2022-03-10 16:32:17 +0300

    ThirdCommit

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -7,11 +7,32 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private bool spawned = false;
+    private float decay;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Reset();
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
         {
+            decay = 1f;
+            spawned = true;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        }
+    }
+    private void Reset()
+    {
+        if (spawned && decay > 0)
+        {
+            decay -= Time.deltaTime;
+        }
+        if (decay < 0)
+        {
+            decay = 0;
+            spawned = false;
         }
     }
 }
+
+
